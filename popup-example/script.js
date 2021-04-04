9f7c5ba82a41fee80028f8be56fd7c25d62c286b
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2021-04-04 14:54:48 -0700

    Added vanilla example

---
+++ popup-example/script.js
@@ -0,0 +1,9 @@
+function popupOpen() {
+  const popup = document.getElementById('popup');
+  popup.style.visibility = 'visible';
+}
+
+function popupClose() {
+  const popup = document.getElementById('popup');
+  popup.style.visibility = 'hidden';
+}
